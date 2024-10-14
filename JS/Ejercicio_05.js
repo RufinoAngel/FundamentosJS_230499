f7572497235c83a0c1249e07db732986340ef980
Angel de Jesus Rufino Mendoza <230499@utxicotepec.edu.mx>
2024-10-14 14:06:43 -0600

    Practica 07
    Agregación de 3 apartados al ejercicio 05,3.-Switch-case,4.-Try/Catch,5.-Break/continue

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -114,3 +114,112 @@ if(fechaActual >=inicioPrimavera && fechaActual< inicioVerano)
     console.log("Evaluando la mayoria de edad de una persona en Esatdos Unidos.")
     console.log(evaluarMayoriaEdad(edadPersona, "US"));
     
+
+    console.log("%c3.- SWITCH - CASE (Elección por valor definido))",  style_console);
+    
+    //Calcualndo tu generaciòn en base a tu edad
+    let anioNacimiento = 2000;
+    let asignaGeneracion= (anioNacimiento)=>{
+    
+    switch(true)
+    {
+        case (anioNacimiento<1968): //Baby Boomers
+        return "Baby Boomers";
+    
+        case(anioNacimiento>1968 && anioNacimiento <=1980):
+        return "Generación X";
+    
+        case(anioNacimiento>1981 && anioNacimiento<=1993):
+        return "Millennials";
+    
+        case(anioNacimiento>1994 && anioNacimiento<=2010):
+        return "Generación Z";
+        
+        case(anioNacimiento>2011 && anioNacimiento<=2024):
+        return "Cristalitos";
+        }
+    }
+    
+    console.log(`Dado que nació en el año 2000 es de la generación : ${asignaGeneracion(2000)}`);
+    console.log("%c4.- Manejo de Excepciones (TRY / CATCH)",  style_console);
+//En algunas ocaciones existiran errores que no son culpa del programa, si no del usuario, la red, el solo incluso de un MIDDLEWARE, pero que sin duda demenos controlar para evitar las fallas de ejecución-
+
+console.log("Lo intentamos dividir: 0/10, el resultado es:")
+
+// El try no interrumpe la ejecuciòn del programa, pero no puede eliminarse el catch
+
+
+try{ //intenta
+    let result = 0/10;  //dividir un entero entre 0
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Lo intentamos dividir: 10/0, el resultado es:")
+
+try{ //intenta
+    let result = 10/0;  //dividir 0 entre un entero
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Lo intentamos dividir: a/10, el resultado es:")
+
+try{ //intenta
+    let result = "a"/0;  //dividir una letra entre 0
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Lo intentamos dividir: la variable a/10, el resultado es:")
+
+try{ //intenta
+    let result = a/0;  //dividir una variable entre 0
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("Lo intentamos dividir: el valor de la variable x/ entre el valor de la variable y, el resultado es:")
+
+try{ //intenta
+    let x=8, y=2,result = x/y;  //dividir una variable entre otra variable
+    console.log(result)
+}
+catch(error)
+{
+    console.log("Ocurrio un error: "+error.message);
+}
+
+console.log("%c5.- Control de Ciclos (BREAK / CONTINUE)",  style_console);
+
+//En algunas ocaciones será importante detener un ciclo de manera abrupta para controlar casos especiales en un ciclo..
+
+console.log("Vamos a contar del 1 al 10...")
+for(let num = 1; num <= 10; num++)
+    console.log(num);
+
+console.log("Ahora necesitamos que si llegas a 7 pares de contar... suponiendo que solo por hoy es de mala suerte....")
+for(num = 1; num <= 10; num++)
+    if(num==7)
+        break; //Deja de hacer lo que esta haciendo y para todo
+    else
+    console.log(num);
+
+    console.log("Ahora necesitamos que si llegas a 7 te saltes ese numero y continues....")
+    for(num = 1; num <= 10; num++){
+        if(num==7){
+            continue;}
+        console.log(num);
+}
